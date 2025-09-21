7d21f4cbbed7f55923690d063d8052aa5085649b
Leivmox <leivmox@gmail.com>
2025-09-21 21:49:41 +0800

    update

---
+++ day03-运算符/day03-code/src/com/mycode/test.java
@@ -0,0 +1,17 @@
+package com.mycode;
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("第一个数字:");
+//        int a = sc.nextInt();
+//        System.out.println("第二个数字:");
+//        int b = sc.nextInt();
+//        System.out.println(a == 6 || b == 6 || (a + b) % 6 == 0);
+//        int i = -5;
+//        System.out.println(i>>>2);
+
+    }
+}
